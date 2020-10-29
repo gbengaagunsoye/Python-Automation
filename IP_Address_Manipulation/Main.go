87488a068128b086acda200ded232c56f93df6f9
gbengaagunsoye <steveso4c@gmail.com>
2020-10-29 11:37:35 -0500

    Sending HTTP Request in Go

--- IP_Address_Manipulation/Main.go
+++ IP_Address_Manipulation/Main.go
@@ -19,10 +19,18 @@ func main() {
 
 	}
 
-	// var counter int8
-	counter := iota
+	var counter int8
+	counter = 0
 
 	for start, end := 0, 10; start < end; start = start + 1 {
 		println(counter)
+		counter = counter + 1
 	}
+
+	entries := []int32{6, 8, 32, 5, 9, 2, 8, 4, 1}
+
+	for _, v := range entries {
+		defer fmt.Printf("%v \n", v)
+	}
+
 }
